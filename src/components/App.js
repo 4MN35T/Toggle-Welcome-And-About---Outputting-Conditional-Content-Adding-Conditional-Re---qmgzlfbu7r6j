4e082c315f4db5cf7d90f523b5dce14c577c2e8f
4MN35T <tanmaykarmore01@gmail.com>
2023-03-06 13:28:49 +0530

    changed js/css

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,17 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
+      if{state===true ? (
+      <div id="welcome-div">{<h1>Welcome to Newton School!!</h1>} </div>) : (
       <div id="about-div">
-        <h1>
+        {<h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
-        </h1>
+        </h1>}
       </div>
-      <button id="toggle">Toggle</button>
+      )}
+      <button id="toggle" onClick={handleToggle}>Toggle</button>
     </div>
   );
 };
